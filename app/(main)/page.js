8bceda75330410ab11bd74f8def752eb859d1bbb
sadikjs <sadikjs11@gmail.com>
2025-05-04 11:07:55 +0600

    land page update

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center bg-[url(../public/assets/bg2.png)] no-repeat bg-center bg-cover text-center">
-      <p className="text-amber-600 text-4xl">Welcome to Kazakhstan</p>
+      <p className="text-white text-4xl">Welcome to Kazakhstan</p>
     </div>
   );
 }
